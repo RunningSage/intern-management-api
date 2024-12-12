e30a8fe6337ee75fb2d39092fb67929d66d19cb7
RunningSage <132100509+RunningSage@users.noreply.github.com>
2024-12-12 19:09:59 +0530

    Add files via upload

--- models/user.js
+++ models/user.js
@@ -11,10 +11,6 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-
-
-//hi
-
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   const salt = await bcrypt.genSalt(10);
